5e1b99c26bfeeb06e815a387e2af4fe0fc48758a
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-06-19 11:28:54 -0300

    Refatoração Tabuada e Sorteio

--- SorteioNumeros/app/src/main/java/com/thifany/sorteionumeros/MainActivity.kt
+++ SorteioNumeros/app/src/main/java/com/thifany/sorteionumeros/MainActivity.kt
@@ -2,21 +2,30 @@ package com.thifany.sorteionumeros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var resultadoSorteio: TextView
+    lateinit var botaoSortear: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultadoSorteio = findViewById(R.id.textResultado)
+        botaoSortear = findViewById(R.id.buttonSortear)
+
+        botaoSortear.setOnClickListener {
+            SortearNumero()
+        }
     }
 
-    fun SortearNumero(view: View){
-        var texto = findViewById(R.id.txtNumero) as TextView
-
+    fun SortearNumero() {
         var numeroSorteado = Random().nextInt(11)
 
-        texto.setText("O número é: $numeroSorteado")
+        resultadoSorteio.text = "O número é: $numeroSorteado"
     }
 }
--- Tabuada/app/src/main/java/com/thifany/tabuada/MainActivity.kt
+++ Tabuada/app/src/main/java/com/thifany/tabuada/MainActivity.kt
@@ -2,28 +2,44 @@ package com.thifany.tabuada
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : AppCompatActivity () {
+
+    lateinit var resultadoTabuada: TextView
+    lateinit var botaoCalcular: Button
+    var numeroTabuada: Int? = null
+
+    override fun onCreate ( savedInstanceState : Bundle? ) {
+        super .onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultadoTabuada = findViewById(R.id.txtResultado)
+        botaoCalcular = findViewById(R.id.btnSortear)
+        botaoCalcular.setOnClickListener {
+            numeroTabuada = findViewById<EditText>(R.id.txtNumero).text.toString().toIntOrNull()
+            CalcularTabuada()
+        }
     }
 
-    fun Calcular(view: View){
-        var resultado = findViewById(R.id.textResultado) as TextView
-        var numero= Integer.parseInt(findViewById<EditText>(R.id.textNumero).text.toString())
+    fun CalcularTabuada () {
         var calculo = 0
-        var tabuada = ""
+        var resultado = ""
 
-        for(i in 1..10){
-            calculo = i * numero
-            tabuada += "$i * $numero = $calculo \n"
+        numeroTabuada?.let{
+            for (i in 1..10) {
+                calculo = it * i
+                resultado += "$numeroTabuada X $i = $calculo \n"
+            }
         }
 
-        resultado.setText(tabuada)
+        resultadoTabuada.text = resultado
     }
 }
